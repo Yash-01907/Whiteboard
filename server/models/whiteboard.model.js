667cb520be8a12ac04e5d359588b5ff62ce5e0ad
Yash Pandey <yashpandey1907@gmail.com>
2025-11-27 08:55:05 +0530

    feat: a new whiteboard data model

---
+++ server/models/whiteboard.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const whiteboardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      default: "Untitled Board",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    // The "Black Box" for shapes
+    elements: [
+      {
+        type: Schema.Types.Mixed,
+        required: true,
+      },
+    ],
+    // NEW: Who else can edit this?
+    // ADVANCED OPTION (Only use if you need roles)
+    collaborators: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        role: { type: String, enum: ["editor", "viewer"], default: "editor" },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Whiteboard = model("Whiteboard", whiteboardSchema);
